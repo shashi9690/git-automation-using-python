bdff966d5e6dc37722ffb14ccdd64094e43c8aae
shashi9690 <shashidharprakash33@gmail.com>
2020-10-28 11:06:51 +0530

    trying reveert

--- file.py
+++ file.py
@@ -6,16 +6,7 @@ if __name__=="__main__":
     status_result = subprocess.check_output(["git","status"]).decode()
     print(status_result)
 <<<<<<< HEAD
-    modified_pattern = r"modified:\s+(.*)"
-    print(re.findall(modified_pattern,status_result))
-    all_files = r".*[a-z0-9]+\.\S+"
-    found_files = (re.findall(all_files,status_result))
-    folders = r"\S*/\s"
 
-    found_folders = re.findall(folders,status_result)
-    for file in found_files:
-        print(file.strip())
-    for folder in found_folders:
-        print(folder.strip())
+>>>>>>> parent of 12664b9... on progress
 =======
 >>>>>>> parent of 12664b9... on progress
